997540750ac2d6aaea81372a252192aeee29767c
jrsanches1 <sanchesjoseruy@gmail.com>
2025-06-03 20:20:15 -0300

    Layout

--- src/routes/Home/HomeBody/index.tsx
+++ src/routes/Home/HomeBody/index.tsx
@@ -1,10 +1,22 @@
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
 
 
 export default function HomeBody() {
 
-    return(
+    return (
         <main>
-            <h2>Pagina Inicial</h2>
+            <section>
+                <div className="container">
+                    <div className="mt30 mb30">
+                        <Card title="Pagina inicial" />
+                    </div>
+                    <div className="dflex">
+                        <Button text="Ver promocao" />
+                    </div>
+                </div>
+            </section>
+
         </main>
     );
 }
